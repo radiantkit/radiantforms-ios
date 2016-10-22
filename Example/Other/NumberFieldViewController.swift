31cb87c22bef551c550c1cc2972cc5e907b75ae9
Simon Strandgaard <neoneye@gmail.com>
2016-10-22 21:35:06 +0200

    Added more number fields

--- Example/Other/NumberFieldViewController.swift
+++ Example/Other/NumberFieldViewController.swift
@@ -4,14 +4,38 @@ import SwiftyFORM
 class NumberFieldViewController: FormViewController {
 	override func populate(_ builder: FormBuilder) {
 		builder.navigationTitle = "NumberField"
-		builder.toolbarMode = .none
-		builder += numberField
+		builder.toolbarMode = .simple
+		builder += numberField0
+		builder += numberField1
+		builder += numberField2
+		builder += numberField3
 	}
 	
-	lazy var numberField: NumberFieldFormItem = {
+	lazy var numberField0: NumberFieldFormItem = {
 		let instance = NumberFieldFormItem()
-		instance.title = "Title"
-		instance.value = "123.456"
+		instance.title = "Field 0"
+		instance.value = "1234"
+		return instance
+	}()
+	
+	lazy var numberField1: NumberFieldFormItem = {
+		let instance = NumberFieldFormItem()
+		instance.title = "Field 1"
+		instance.value = "1234.5"
+		return instance
+	}()
+	
+	lazy var numberField2: NumberFieldFormItem = {
+		let instance = NumberFieldFormItem()
+		instance.title = "Field 2"
+		instance.value = "1234.56"
+		return instance
+	}()
+	
+	lazy var numberField3: NumberFieldFormItem = {
+		let instance = NumberFieldFormItem()
+		instance.title = "Field 3"
+		instance.value = "1234.567"
 		return instance
 	}()
 }
