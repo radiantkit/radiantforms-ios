70f0f9eca703363801441254ad9d2eedae9677e3
Simon Strandgaard <strx@sdc.dk>
2019-04-08 12:57:16 +0200

    Extended the ButtonsViewController so the new isHidden booleans gets excercised

--- Example/Other/ButtonsViewController.swift
+++ Example/Other/ButtonsViewController.swift
@@ -6,36 +6,57 @@ class ButtonsViewController: FormViewController {
 	override func populate(_ builder: FormBuilder) {
 		builder.navigationTitle = "Buttons"
 		builder.toolbarMode = .none
-		builder += SectionHeaderTitleFormItem().title("Table Row Buttons")
-		builder += button0
-		builder += button1
-		builder += button2
+		builder += SectionHeaderTitleFormItem().title("Toggle Buttons")
+        builder += toggleButton0
+        builder += toggleButton1
+
+        builder += SectionHeaderTitleFormItem().title("Alert Buttons")
+		builder += alertButton0
+		builder += alertButton1
 	}
 
-	lazy var button0: ButtonFormItem = {
-		let instance = ButtonFormItem()
-		instance.title = "Button 0"
-		instance.action = { [weak self] in
-			self?.form_simpleAlert("Button 0", "Button clicked")
-		}
-		return instance
-		}()
+    lazy var toggleButton0: ButtonFormItem = {
+        let instance = ButtonFormItem()
+        instance.title = "Toggle Button 0"
+        instance.action = { [weak self] in
+            guard let buttonFormItem: ButtonFormItem = self?.alertButton0 else {
+                return
+            }
+            buttonFormItem.isHidden = !buttonFormItem.isHidden
+            self?.reloadForm()
+        }
+        return instance
+    }()
+    
+    lazy var toggleButton1: ButtonFormItem = {
+        let instance = ButtonFormItem()
+        instance.title = "Toggle Button 1"
+        instance.action = { [weak self] in
+            guard let buttonFormItem: ButtonFormItem = self?.alertButton1 else {
+                return
+            }
+            buttonFormItem.isHidden = !buttonFormItem.isHidden
+            self?.reloadForm()
+        }
+        return instance
+    }()
+    
+    lazy var alertButton0: ButtonFormItem = {
+        let instance = ButtonFormItem()
+        instance.title = "Button 0"
+        instance.action = { [weak self] in
+            self?.form_simpleAlert("Button 0", "Button clicked")
+        }
+        return instance
+    }()
+    
+    lazy var alertButton1: ButtonFormItem = {
+        let instance = ButtonFormItem()
+        instance.title = "Button 1"
+        instance.action = { [weak self] in
+            self?.form_simpleAlert("Button 1", "Button clicked")
+        }
+        return instance
+    }()
 
-	lazy var button1: ButtonFormItem = {
-		let instance = ButtonFormItem()
-		instance.title = "Button 1"
-		instance.action = { [weak self] in
-			self?.form_simpleAlert("Button 1", "Button clicked")
-		}
-		return instance
-		}()
-
-	lazy var button2: ButtonFormItem = {
-		let instance = ButtonFormItem()
-		instance.title = "Button 2"
-		instance.action = { [weak self] in
-			self?.form_simpleAlert("Button 2", "Button clicked")
-		}
-		return instance
-		}()
 }
