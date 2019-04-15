dffafed097ea21d34cef17dbd5393d5ed6077b31
Simon Strandgaard <strx@sdc.dk>
2019-04-15 14:55:28 +0200

    Moved the AmountTextFieldFormItemCellModel class, so that "AmountTextFieldFormItem.swift" is consistent with the "TextFieldFormItem.swift"

--- Source/Cells/AmountTextFieldCell.swift
+++ Source/Cells/AmountTextFieldCell.swift
@@ -1,6 +1,22 @@
 // MIT license. Copyright (c) 2019 SwiftyFORM. All rights reserved.
 import UIKit
 
+public struct AmountTextFieldFormItemCellModel {
+    var title: String = ""
+    var toolbarMode: ToolbarMode = .simple
+    var placeholder: String = ""
+    var returnKeyType: UIReturnKeyType = .default
+    var model: AmountTextFieldFormItem! = nil
+    
+    var valueDidChange: (String) -> Void = { (value: String) in
+        SwiftyFormLog("value \(value)")
+    }
+    
+    var didEndEditing: (String) -> Void = { (value: String) in
+        SwiftyFormLog("value \(value)")
+    }
+}
+
 public class AmountTextFieldCell: UITableViewCell {
     
 }
--- Source/FormItems/AmountTextFieldFormItem.swift
+++ Source/FormItems/AmountTextFieldFormItem.swift
@@ -1,22 +1,6 @@
 // MIT license. Copyright (c) 2019 SwiftyFORM. All rights reserved.
 import Foundation
 
-public struct AmountTextFieldFormItemCellModel {
-    var title: String = ""
-    var toolbarMode: ToolbarMode = .simple
-    var placeholder: String = ""
-    var returnKeyType: UIReturnKeyType = .default
-    var model: AmountTextFieldFormItem! = nil
-    
-    var valueDidChange: (String) -> Void = { (value: String) in
-        SwiftyFormLog("value \(value)")
-    }
-    
-    var didEndEditing: (String) -> Void = { (value: String) in
-        SwiftyFormLog("value \(value)")
-    }
-}
-
 public class AmountTextFieldFormItem: FormItem {
     override func accept(visitor: FormItemVisitor) {
         visitor.visit(object: self)
