c95e1bab949032de01fff9eaeb8bc41946f2b2ca
Simon Strandgaard <strx@sdc.dk>
2019-04-15 14:45:33 +0200

    Adapted TextFieldFormItemCellModel for the AmountTextField.

--- Source/FormItems/AmountTextFieldFormItem.swift
+++ Source/FormItems/AmountTextFieldFormItem.swift
@@ -1,9 +1,143 @@
 // MIT license. Copyright (c) 2019 SwiftyFORM. All rights reserved.
 import Foundation
 
+public struct AmountTextFieldFormItemCellModel {
+    var title: String = ""
+    var toolbarMode: ToolbarMode = .simple
+    var placeholder: String = ""
+    var returnKeyType: UIReturnKeyType = .default
+    var model: AmountTextFieldFormItem! = nil
+    
+    var valueDidChange: (String) -> Void = { (value: String) in
+        SwiftyFormLog("value \(value)")
+    }
+    
+    var didEndEditing: (String) -> Void = { (value: String) in
+        SwiftyFormLog("value \(value)")
+    }
+}
+
 public class AmountTextFieldFormItem: FormItem {
     override func accept(visitor: FormItemVisitor) {
         visitor.visit(object: self)
     }
-}
+    
+    public var returnKeyType: UIReturnKeyType = .default
+    
+    @discardableResult
+    public func returnKeyType(_ returnKeyType: UIReturnKeyType) -> Self {
+        self.returnKeyType = returnKeyType
+        return self
+    }
+    
+    public typealias SyncBlock = (_ value: String) -> Void
+    public var syncCellWithValue: SyncBlock = { (string: String) in
+        SwiftyFormLog("sync is not overridden")
+    }
+    
+    internal var innerValue: String = ""
+    public var value: String {
+        get {
+            return self.innerValue
+        }
+        set {
+            self.assignValueAndSync(newValue)
+        }
+    }
+    
+    public typealias TextDidChangeBlock = (_ value: String) -> Void
+    public var textDidChangeBlock: TextDidChangeBlock = { (value: String) in
+        SwiftyFormLog("not overridden")
+    }
+    
+    public func textDidChange(_ value: String) {
+        innerValue = value
+        textDidChangeBlock(value)
+    }
+    
+    public typealias TextEditingEndBlock = (_ value: String) -> Void
+    public var textEditingEndBlock: TextEditingEndBlock = { (value: String) in
+        SwiftyFormLog("not overridden")
+    }
+    
+    public func editingEnd(_ value: String) {
+        textEditingEndBlock(value)
+    }
+    
+    public func assignValueAndSync(_ value: String) {
+        innerValue = value
+        syncCellWithValue(value)
+    }
+    
+    public var reloadPersistentValidationState: () -> Void = {}
+    
+    public var obtainTitleWidth: () -> CGFloat = {
+        return 0
+    }
+    
+    public var assignTitleWidth: (CGFloat) -> Void = { (width: CGFloat) in
+        // do nothing
+    }
+    
+    public var placeholder: String = ""
+    
+    @discardableResult
+    public func placeholder(_ placeholder: String) -> Self {
+        self.placeholder = placeholder
+        return self
+    }
+    
+    public var title: String = ""
+    
+    @discardableResult
+    public func title(_ title: String) -> Self {
+        self.title = title
+        return self
+    }
 
+    public let validatorBuilder = ValidatorBuilder()
+    
+    @discardableResult
+    public func validate(_ specification: Specification, message: String) -> Self {
+        validatorBuilder.hardValidate(specification, message: message)
+        return self
+    }
+    
+    @discardableResult
+    public func softValidate(_ specification: Specification, message: String) -> Self {
+        validatorBuilder.softValidate(specification, message: message)
+        return self
+    }
+    
+    @discardableResult
+    public func submitValidate(_ specification: Specification, message: String) -> Self {
+        validatorBuilder.submitValidate(specification, message: message)
+        return self
+    }
+    
+    @discardableResult
+    public func required(_ message: String) -> Self {
+        submitValidate(CountSpecification.min(1), message: message)
+        return self
+    }
+    
+    public func liveValidateValueText() -> ValidateResult {
+        return validatorBuilder.build().liveValidate(self.value)
+    }
+    
+    public func liveValidateText(_ text: String) -> ValidateResult {
+        return validatorBuilder.build().validate(text, checkHardRule: true, checkSoftRule: true, checkSubmitRule: false)
+    }
+    
+    public func submitValidateValueText() -> ValidateResult {
+        return validatorBuilder.build().submitValidate(self.value)
+    }
+    
+    public func submitValidateText(_ text: String) -> ValidateResult {
+        return validatorBuilder.build().validate(text, checkHardRule: true, checkSoftRule: true, checkSubmitRule: true)
+    }
+    
+    public func validateText(_ text: String, checkHardRule: Bool, checkSoftRule: Bool, checkSubmitRule: Bool) -> ValidateResult {
+        return validatorBuilder.build().validate(text, checkHardRule: checkHardRule, checkSoftRule: checkSoftRule, checkSubmitRule: checkSubmitRule)
+    }
+}
