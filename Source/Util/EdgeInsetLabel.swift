2ae4b874f16247a6dfaaa29d209edb03843cdad9
Simon Strandgaard <strx@sdc.dk>
2019-04-16 12:27:03 +0200

    EdgeInsetLabel class added

---
+++ Source/Util/EdgeInsetLabel.swift
@@ -0,0 +1,25 @@
+// MIT license. Copyright (c) 2019 SwiftyFORM. All rights reserved.
+import UIKit
+
+/// UILabel with padding, inspired by:
+/// https://stackoverflow.com/questions/21167226/resizing-a-uilabel-to-accommodate-insets/21267507#21267507
+/// https://stackoverflow.com/a/55437367/78336
+class EdgeInsetLabel: UILabel {
+    var edgeInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = bounds.inset(by: edgeInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -edgeInsets.top,
+                                          left: -edgeInsets.left,
+                                          bottom: -edgeInsets.bottom,
+                                          right: -edgeInsets.right)
+        return textRect.inset(by: invertedInsets)
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: edgeInsets))
+    }
+}
