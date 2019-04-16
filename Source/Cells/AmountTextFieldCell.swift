b9622beedb9e8bc74e3272b4e4b947b48b60872d
Simon Strandgaard <strx@sdc.dk>
2019-04-16 15:56:00 +0200

    AmountTextFieldCell now lets the user type in numbers that always has 2 decimals places.

--- Source/Cells/AmountTextFieldCell.swift
+++ Source/Cells/AmountTextFieldCell.swift
@@ -409,13 +409,37 @@ public class AmountTextFieldCell: UITableViewCell {
 extension AmountTextFieldCell: UITextFieldDelegate {
     public func textFieldDidBeginEditing(_ textField: UITextField) {
         updateToolbarButtons()
+        textField.text = textField.text?.trimmingCharacters(in: CharacterSet.whitespaces)
     }
     
     public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let textFieldString: NSString = textField.text as NSString? ?? ""
-        let s = textFieldString.replacingCharacters(in: range, with:string)
-        let valid = validateAndUpdateErrorIfNeeded(s, shouldInstallTimer: true, checkSubmitRule: false)
-        return valid
+        let currentText: String = textField.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else {
+            SwiftyFormLog("Unable to create Range from NSRange")
+            return false
+        }
+        
+        let updatedText0: String = currentText.replacingCharacters(in: stringRange, with: string)
+        let updatedText1: String = updatedText0.replacingOccurrences(of: ",", with: "")
+        let updatedText2: String = updatedText1.replacingOccurrences(of: ".", with: "")
+        let updatedText: String = updatedText2
+
+        guard let uint64Value: UInt64 = UInt64(updatedText) else {
+            return false
+        }
+        if uint64Value == 0 {
+            textField.text = nil
+            return false
+        }
+        let decimal0: Decimal = Decimal(uint64Value)
+        let decimal1: Decimal = Decimal(integerLiteral: 100)
+        let decimal2: Decimal = decimal0 / decimal1
+        
+        let formatter: NumberFormatter = AmountFormatter.shared
+        let s: String = formatter.string(from: decimal2 as NSNumber) ?? ""
+        
+        textField.text = s
+        return false
     }
     
     // Hide the keyboard when the user taps the return key in this UITextField
@@ -436,5 +460,28 @@ extension AmountTextFieldCell: CellHeightProvider {
         let value = sizes.cellHeight
         //SwiftyFormLog("compute height of row: \(value)")
         return value
+    }
+}
+
+fileprivate class AmountFormatter: NumberFormatter {
+    static let shared = AmountFormatter()
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init() {
+        super.init()
+        self.numberStyle = .decimal
+        self.currencyCode = ""
+        self.currencySymbol = ""
+        self.currencyGroupingSeparator = ""
+        self.perMillSymbol = ""
+        self.groupingSeparator = ""
+        self.minimumFractionDigits = 2
+        self.maximumFractionDigits = 2
+        self.negativeSuffix = ""
+        self.negativePrefix = "-"
+        self.decimalSeparator = "."
     }
 }
