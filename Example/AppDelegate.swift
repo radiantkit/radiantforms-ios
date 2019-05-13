9af384955dc764595917db3345ac135ba16443c0
Simon Strandgaard <strx@sdc.dk>
2019-05-13 09:52:55 +0200

    Experiments styling the UILabel inside the UITableView section header/footer

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -10,9 +10,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        UITextField.appearance().tintColor = UIColor.purple
 //        UITextField.appearance(whenContainedInInstancesOf: [AmountCell.self]).tintColor = UIColor.purple
 //        UITextField.appearance(whenContainedInInstancesOf: [AmountCell.self]).textColor = UIColor.green
-        UITableViewCell.appearance().backgroundColor = UIColor.gray
-        UITableView.appearance().separatorColor = UIColor.white
+        UITableViewCell.appearance().backgroundColor = UIColor(white: 0.95, alpha: 1.0)
+        UITableView.appearance().backgroundColor = UIColor(white: 1.0, alpha: 1.0)
+        UITableView.appearance().separatorColor = UIColor(white: 0.8, alpha: 1.0)
         UILabel.appearance().textColor = .red
+//        UITableViewHeaderFooterView.appearance().backgroundColor = UIColor.brown
+        UITableViewHeaderFooterView.appearance().tintColor = UIColor.green
+        UILabel.appearance(whenContainedInInstancesOf: [UITableViewHeaderFooterView.self]).textColor = UIColor.green
+        UILabel.appearance(whenContainedInInstancesOf: [UITableViewHeaderFooterView.self]).tintColor = UIColor.blue
 
         
 //        AmountCell.configureAppearance(whenContainedInInstancesOf: [])
