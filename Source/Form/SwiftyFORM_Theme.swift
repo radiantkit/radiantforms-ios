6e00ad8bbbe2b9517472f1ebbcd6259ce093d1a4
Simon Strandgaard <strx@sdc.dk>
2019-05-20 09:42:56 +0200

    Extracted Theme struct to separate file

---
+++ Source/Form/SwiftyFORM_Theme.swift
@@ -0,0 +1,127 @@
+// MIT license. Copyright (c) 2019 SwiftyFORM. All rights reserved.
+
+public struct SwiftyFORM_Theme {
+    internal let tableViewBackground: SwiftyFORM_Theme_TableViewBackground
+    internal let tableViewSeparator: SwiftyFORM_Theme_TableViewSeparator
+    internal let sectionHeader: SwiftyFORM_Theme_SectionHeader
+    internal let sectionFooter: SwiftyFORM_Theme_SectionFooter
+    internal let cellBackground: SwiftyFORM_Theme_CellBackground
+    
+    public static func lightTheme() -> SwiftyFORM_Theme {
+        return SwiftyFORM_Theme(
+            tableViewBackground: .lightTheme,
+            tableViewSeparator: .lightTheme,
+            sectionHeader: .lightTheme,
+            sectionFooter: .lightTheme,
+            cellBackground: .lightTheme
+        )
+    }
+    
+    public static func darkTheme() -> SwiftyFORM_Theme {
+        return SwiftyFORM_Theme(
+            tableViewBackground: .darkTheme,
+            tableViewSeparator: .darkTheme,
+            sectionHeader: .darkTheme,
+            sectionFooter: .darkTheme,
+            cellBackground: .darkTheme
+        )
+    }
+    
+    public static func useSystemDefaultTheme() -> SwiftyFORM_Theme {
+        return SwiftyFORM_Theme(
+            tableViewBackground: .useSystemDefault,
+            tableViewSeparator: .useSystemDefault,
+            sectionHeader: .useSystemDefault,
+            sectionFooter: .useSystemDefault,
+            cellBackground: .useSystemDefault
+        )
+    }
+}
+
+internal enum SwiftyFORM_Theme_TableViewBackground {
+    case useSystemDefault, lightTheme, darkTheme
+}
+
+internal enum SwiftyFORM_Theme_TableViewSeparator {
+    case useSystemDefault, lightTheme, darkTheme
+}
+
+internal enum SwiftyFORM_Theme_SectionHeader {
+    case useSystemDefault, lightTheme, darkTheme
+}
+
+internal enum SwiftyFORM_Theme_SectionFooter {
+    case useSystemDefault, lightTheme, darkTheme
+}
+
+internal enum SwiftyFORM_Theme_CellBackground {
+    case useSystemDefault, lightTheme, darkTheme
+}
+
+
+internal extension SwiftyFORM_Theme_SectionHeader {
+    var textLabel_textColor: UIColor? {
+        switch self {
+        case .useSystemDefault:
+            return nil
+        case .lightTheme:
+            return UIColor.darkGray
+        case .darkTheme:
+            return UIColor.white
+        }
+    }
+}
+
+internal extension SwiftyFORM_Theme_SectionFooter {
+    var textLabel_textColor: UIColor? {
+        switch self {
+        case .useSystemDefault:
+            return nil
+        case .lightTheme:
+            return UIColor.lightGray
+        case .darkTheme:
+            return UIColor.white
+        }
+    }
+}
+
+internal extension SwiftyFORM_Theme_TableViewBackground {
+    var color: UIColor? {
+        switch self {
+        case .useSystemDefault:
+            return nil
+        case .lightTheme:
+            return UIColor(white: 0.9, alpha: 1.0)
+        case .darkTheme:
+            return UIColor(white: 0.2, alpha: 1.0)
+        }
+    }
+}
+
+internal extension SwiftyFORM_Theme_TableViewSeparator {
+    var color: UIColor? {
+        switch self {
+        case .useSystemDefault:
+            return nil
+        case .lightTheme:
+            return UIColor(white: 0.8, alpha: 1.0)
+        case .darkTheme:
+            return UIColor(white: 0.4, alpha: 1.0)
+        }
+    }
+}
+
+internal extension SwiftyFORM_Theme_CellBackground {
+    var color: UIColor? {
+        switch self {
+        case .useSystemDefault:
+            return nil
+        case .lightTheme:
+            return UIColor(white: 0.95, alpha: 1.0)
+        case .darkTheme:
+            return UIColor(white: 0.3, alpha: 1.0)
+        }
+    }
+}
+
+
