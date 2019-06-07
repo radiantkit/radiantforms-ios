6963c1155756d4b114d6d932e82fc40299b7e672
Simon Strandgaard <strx@sdc.dk>
2019-06-07 15:34:23 +0200

    Changed prefix from `form_` to `rf_`

--- Source/Util/Bundle+LoadView.swift
+++ Source/Util/Bundle+LoadView.swift
@@ -2,26 +2,34 @@
 import Foundation
 
 extension Bundle {
-	public enum FormLoadViewError: Error {
+	public enum RFLoadViewError: Error {
 		case expectedXibToExistButGotNil
 		case expectedXibToContainJustOneButGotDifferentNumberOfObjects
 		case xibReturnedWrongType
 	}
 
+    @available(*, unavailable, renamed: "RFLoadViewError")
+    typealias FormLoadViewError = RFLoadViewError
+
 	/* 
 	usage:
-	let cell: ContactPickerCell = try Bundle.main.form_loadView("ContactPickerCell")
+	let cell: ContactPickerCell = try Bundle.main.rf_loadView("ContactPickerCell")
 	*/
-	public func form_loadView<T>(_ name: String) throws -> T {
+	public func rf_loadView<T>(_ name: String) throws -> T {
 		guard let topLevelObjects = loadNibNamed(name, owner: self, options: nil) else {
-			throw FormLoadViewError.expectedXibToExistButGotNil
+			throw RFLoadViewError.expectedXibToExistButGotNil
 		}
 		guard topLevelObjects.count == 1 else {
-			throw FormLoadViewError.expectedXibToContainJustOneButGotDifferentNumberOfObjects
+			throw RFLoadViewError.expectedXibToContainJustOneButGotDifferentNumberOfObjects
 		}
 		guard let result = topLevelObjects.first as? T else {
-			throw FormLoadViewError.xibReturnedWrongType
+			throw RFLoadViewError.xibReturnedWrongType
 		}
 		return result
 	}
+
+    @available(*, deprecated, message: "Will be removed with Version2, use rf_loadView instead")
+    public func form_loadView<T>(_ name: String) throws -> T {
+        return try rf_loadView(name)
+    }
 }
